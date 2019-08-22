7586035b863655688a2993378d6021f88dd118cd
Kuldeep <kuldeepghatol@gmail.com>
2019-08-22 23:39:40 +0530

    Added the comments in the class

--- app/src/main/java/com/resocoder/databinding/MainActivity.kt
+++ app/src/main/java/com/resocoder/databinding/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.resocoder.databinding
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
@@ -9,6 +8,10 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.resocoder.databinding.databinding.ActivityMainBinding
 
+/**
+ * Class MainActity
+ * This is main class for application
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
